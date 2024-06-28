3c52984af4f253b78e9eaa352f92b5237a17a2ea
Maka <maka8295@gmail.com>
2024-06-29 11:25:24 +1200

    added ghosts to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    echo -e "\033[0;31m󰊠 \033[0m""\033[0;33m󰊠 \033[0m""\033[0;32m󰊠 \033[0m""\033[0;35m󰊠 \033[0m""\033[0;34m󰊠 \033[0m""\033[0;36m󰊠 \033[0m"
+   
+
+
+
+
+
 end
 
 ## nvim transparency
@@ -11,6 +19,6 @@ function nvim
     command nvim $argv
 
     # Restore the original background opacity when Neovim exits
-    kitty @ set-background-opacity 0.8
+    kitty @ set-background-opacity 1
 end
 
